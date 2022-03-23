508443bf73bc0bdd0d450a222385515385819d58
navono <navono007@gmail.com>
2022-03-23 14:55:52 +0800

    feat: mobx

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
+import { Container, Divider, Header, List } from "semantic-ui-react";
+import { configure } from "mobx";
+import { configurePersistable } from "mobx-persist-store";
+
+// https://mobx.js.org/configuration.html#configuration-
+configure({
+  enforceActions: "always",
+  computedRequiresReaction: true,
+  reactionRequiresObservable: true,
+  observableRequiresReaction: true,
+  disableErrorBoundaries: false
+});
+configurePersistable({
+  debugMode: false
+});
+
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href =
+  "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+document.head.appendChild(styleLink);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Container style={{ margin: 20 }}>
+    <Header as="h1" dividing>
+      Mobx Persist Store with MobX 6 (v1.0)
+    </Header>
+    <List bulleted>
+      <List.Item
+        as="a"
+        content="Official Documentation"
+        href="https://github.com/quarrant/mobx-persist-store"
+        target="_blank"
+      />
+    </List>
+    <Divider hidden />
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Container>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
